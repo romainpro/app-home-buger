0a63e7959aec2d37d4396f8da99f4a20aa487a3f
romain.casagrande <romain.casagrande.pro@gamil.com>
2023-08-14 22:58:34 +0200

    placement-provisional

--- src/components/marketing/pagesSections/Header.js
+++ src/components/marketing/pagesSections/Header.js
@@ -4,8 +4,13 @@ import Logo from '../../../media/svg/Logo-burger-house.svg'
 
 export default function Header() {
   return (
-    <div>
+    <div className='flex items-center justify-between py-10 bg-gray-500'>
+        <div className='bg-red-500 h-20 w-full'>
         <img src={Logo} alt='burger' className='w-64'/>
+        </div>
+        <div className='bg-green-500 h-20 w-full'>
+        
+        </div>
     </div>
   )
 }
